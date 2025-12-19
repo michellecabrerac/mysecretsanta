c0e3a416ae794e983b057f7cc7558077c401ddd0
michellecabrerac <michellecabrerac@gmail.com>
2025-12-19 17:56:05 +0100

    Initial commit My Secret Santa

---
+++ src/main/java/com/michellecabrerac/mysecretsanta/MysecretsantaApplication.java
@@ -0,0 +1,13 @@
+package com.michellecabrerac.mysecretsanta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysecretsantaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysecretsantaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michellecabrerac/mysecretsanta/MysecretsantaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michellecabrerac.mysecretsanta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysecretsantaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
